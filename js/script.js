d94543f7592e04beee8b9fdd086c23faf39b1a6f
Paulina Bociaga <paulinabociaga@gmail.com>
2020-01-20 20:11:25 +0100

    add function prompt

--- js/script.js
+++ js/script.js
@@ -16,3 +16,21 @@ else if (playerInput == '2'){
 else if (playerInput == '3'){
 	computerMove = 'nożyce'
 }
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+else if (playerInput == '2'){
+	computerMove = 'papier'
+}
+else if (playerInput == '3'){
+	computerMove = 'nożyce'
+}
