1d1797732a9e3900788017b8e4f891847fe455db
Paulina Bociaga <paulinabociaga@gmail.com>
2020-01-20 19:56:49 +0100

    Add computer movement

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-let computerMove = ' kamień';
-printMessage('Zagrałem' +  computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Twój ruch to: ' + playerMove);
 
-let playerMove = ' papier';
-printMessage('Zagrałem' +  playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+else if (playerInput == '2'){
+	computerMove = 'papier'
+}
+else if (playerInput == '3'){
+	computerMove = 'nożyce'
+}
